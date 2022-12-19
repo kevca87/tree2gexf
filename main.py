da538adc567af92214554b419498cc8ad7010b90
Kevin <kevcespedesarancibia87@gmail.com>
2022-12-18 23:17:57 -0400

    reading all files from dir

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import os
 import networkx as nx
 import xml.etree.ElementTree as et
+
+OUT_DIR_GRAPHS = 'gexf'
+IN_DIR_TREES = 'trees'
 
 def get_xml_tree(file):
     tree = et.parse(file)
@@ -19,6 +23,13 @@ def tree_2_gexf(tree,graph=None):
         graph = tree_2_gexf(node,graph)
     return graph
 
-tree = get_xml_tree('trees/vert.x-4.0.0_benchmarks.xml')
-G = tree_2_gexf(tree)
-nx.write_gexf(G, "geeksforgeeks.gexf")
+def trees_2_graphs(in_dir_trees = IN_DIR_TREES,out_dir_graphs = OUT_DIR_GRAPHS):
+    trees_files_xml = [t.split('.xml')[0] for t in os.listdir(in_dir_trees) if '.xml' in t]
+    print(trees_files_xml)
+    for tree_file in trees_files_xml:
+        tree = get_xml_tree(f'{in_dir_trees}/{tree_file}.xml')
+        graph = tree_2_gexf(tree)
+        nx.write_gexf(graph,f'{out_dir_graphs}/{tree_file}.gexf')
+
+if __name__ == '__main__':
+    trees_2_graphs()
